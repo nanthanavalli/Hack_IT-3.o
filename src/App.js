29ad4648988b3265db2471cfc0939400c9bbe848
Naveen Akash <naveen.akash0904@gmail.com>
2024-03-13 13:30:03 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Domain from "./pages/Domain";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <Home/>
     </div>
   );
 }
