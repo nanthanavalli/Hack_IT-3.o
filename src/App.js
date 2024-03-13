947cf07810d81bba4ae2860b63e078420047bc31
nanthanavalli <nanthanavalli0309@gmail.com>
2024-03-13 21:20:55 +0530

    "About page updated"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Domain from "./pages/Domain";
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <About />
       <Domain />
     </div>
   );
